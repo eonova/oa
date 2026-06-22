e026de18542f97bbd1481cebc115a0ea0f0674f1
eonova <hi@eonova.me>
2026-06-23 02:05:29 +0800

    first commit

--- apps/miniapp/app.ts
+++
@@ -1,6 +0,0 @@
-App({
-  globalData: {
-    apiBase: 'http://127.0.0.1:3001',
-    currentUserId: 'u-2',
-  },
-})
--- apps/web/src/main.tsx
+++
@@ -1,5 +0,0 @@
-import { createApp } from 'vue'
-import App from './App'
-import './styles.css'
-
-createApp(App).mount('#app')
